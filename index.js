1642bfe38cc4ab6b8068f7c99467e7ea182340fc
cvegah <cesar.vega.hdez@gmail.com>
2021-11-27 20:42:59 -0600

    Installing libraries
    express, nodemon, ejs

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// DB
+// Settings
+// Middleware
+// Routes
+// Static files
+
+app.listen(PORT, () => {
+    console.log(`Server runnning on http://localhost:${PORT}`);
+});
